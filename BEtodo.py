c6ddb57bed94d95475be05fbe1267d323b3d2d07
thalesapenas <thales.rodrigues@ufpi.edu.br>
2024-04-25 19:49:49 -0300

    atualização de semântica e clean code

--- BEtodo.py
+++ BEtodo.py
@@ -8,84 +8,73 @@ from fastapi.middleware import Middleware
 middleware = [
     Middleware(
         CORSMiddleware,
-        allow_origins=[''],
+        allow_origins=["*"],
         allow_credentials=True,
-        allow_methods=[''],
-        allow_headers=['*']
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
 ]
 
 app = FastAPI(middleware=middleware)
 
 
+class Task(BaseModel):
+    id: Optional[str] = None
+    name: str
+    isDone: bool = False
 
-class task(BaseModel):
-    id: Optional[str]= None
-    nome: str
-    is_done: bool = False
 
-banco: List[task] = []
+banco: List[Task] = []
 
-@app.get("/") # listar tarefas
-def bem_vindo():
-    return "fala, izaias! bem vindo ao Back end do to-do!"
 
-@app.get("/task") # listar tarefas
-def lista_de_tarefas():
+@app.get("/")
+def hello():
+    return "Server is running!"
+
+
+@app.get("/tasks")
+def get_tasks():
     return banco
 
-@app.get("/task/{task_id}")# buscar tarefa especifica
-def find_task(task_id: str):
+
+@app.get("/tasks/progress")
+def get_tasks_progress():
+    done = 0
+    total = 0
+    if len(banco) > 0:
+        total = len(banco)
+        for task in banco:
+            if task.isDone == True:
+                done += 1
+    return {"done": done, "total": total}
+
+
+@app.post("/tasks")
+def create_task(newTask: Task):
     for task in banco:
-        if task.id == task_id:
+        if task.name == newTask.name:
+            return "A tarefa já existe!"
+    newTask.id = str(uuid4())
+    banco.append(newTask)
+    return newTask
+
+
+@app.delete("/tasks/{taskId}")
+def delete_task(taskId: str):
+    for task in banco:
+        if task.id == taskId:
+            banco.remove(task)
+            return "Tarefa removida!"
+    return "A tarefa não existe!"
+
+
+@app.patch("/tasks/{taskId}")
+def toggle_task_is_done(taskId: str):
+    for task in banco:
+        if task.id == taskId:
+            if task.isDone == True:
+                task.isDone = False
+            else:
+                task.isDone = True
             return task
     return "A tarefa não existe!"
-
-@app.delete("/task/{task_id},")#excluir tarefa
-def remove_task(task_id: str):
-    for task in banco:
-        if task.id == task_id:
-            banco.remove(task)
-            return "tarefa removida!"
-    return "A tarefa não existe!"
-
-@app.post("/task")#criar tarefa
-def create_task(task1: task):
-    for task in banco:
-        if task.nome == task1.nome:
-            return "A tarefa já existe!"
-    task1.id = str(uuid4())
-    banco.append(task1)
-    return "tarefa criada com sucesso"
-
-@app.put("/task_update_name/{task_id}")#atualizar tarefa nome da tarefa
-def update(task_id:str, task_nome:str):
-    for task in banco:
-        if task.id == task_id:
-            break
-        return "A tarefa não existe!"
-    task.nome = task_nome
-    return "Tarefa atualizada com sucesso!"
-
-
-@app.put("/task_update_True/{task_id}")#atualizar status da tarefa pra false
-def update(task_id:str):
-    for task in banco:
-        if task.id == task_id:
-            break
-        return "A tarefa não existe!"
-    task.is_done = True
-    return "Tarefa atualizada com sucesso!"
-
-@app.put("/task_update_False/{task_id}")#atualizar status da tarefa pra true
-def update(task_id:str):
-    for task in banco:
-        if task.id == task_id:
-            break
-        return "A tarefa não existe!"
-    task.is_done = False
-    return "Tarefa atualizada com sucesso!"
-
-
-
-
